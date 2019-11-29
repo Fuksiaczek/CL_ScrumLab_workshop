e7f79e7ebfb5e4b28aae4e843eae9863e03c4ad9
MMarek <mildnermarek@gmail.com>
2019-11-29 23:03:24 +0100

    do oceny z brakami w js

---
+++ development/js/plans.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+.fas.fa-plus-square.addEventListener("click", function (){
+}});   //niezrozumiały błąd
+
+
+
+
+const AddList = document.querySelector('.fa-plus-square');
+
+AddList.addEventListener('click', function() {
+    document.querySelector('??? ').style.display = 'flex';
+//brak pomysłu na źródło wyświetlania
+
+
+const editButton = document.querySelectorAll(".fa-edit");
+editButton.addEventListener('click', function () {
+//brak ciała funkcji
+}
+});
+
+const deleteButton = document.querySelectorAll(".fa-trash-alt");
+
